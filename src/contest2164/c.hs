8dad91a5d0238de59a8f2751ddad047a55016f29
Kawin Nikomborirak <knikomborirak@icloud.com>
2025-11-18 20:33:35 -0800

    feat: add working problem 2164c

--- src/contest2164/c.hs
+++ src/contest2164/c.hs
@@ -1,20 +1,62 @@
 module Main (main) where
 
-import Control.Monad (replicateM_)
-import Data.List (sort, sortOn)
+import Control.Monad (guard, replicateM_)
+import qualified Data.ByteString.Char8 as C
+import Data.List (partition, sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Data.Ord (Down (Down))
+import Numeric.Natural (Natural)
 
-killMonsters :: (Ord a, Num b) => a -> [(a, a)] -> b
-killMonsters _ [] = 0
-killMonsters power ((monsterHp, monsterSword) : rest)
-  | power >= monsterHp = 1 + killMonsters (max power monsterSword) rest
-  | otherwise = killMonsters power rest
+removeSword :: (Ord a) => a -> Map a Natural -> Map a Natural
+removeSword = Map.update ((*>) <$> guard . (> 1) <*> Just . subtract 1)
+
+addSword :: (Ord a) => a -> Map a Natural -> Map a Natural
+addSword = flip (Map.insertWith (+)) 1
+
+killMonstersZeroSword' :: (Ord a, Num c) => a -> (c, Map a Natural) -> (c, Map a Natural)
+killMonstersZeroSword' monsterHp (currentKills, currentSwords) =
+  maybe
+    (currentKills, currentSwords)
+    ((,) (currentKills + 1) . flip removeSword currentSwords . fst)
+    (Map.lookupGE monsterHp currentSwords)
+
+killMonstersZeroSword :: (Ord a, Num c, Foldable f) => Map a Natural -> f a -> c
+killMonstersZeroSword =
+  (fst .)
+    . foldr killMonstersZeroSword'
+    . (,) 0
+
+killNonZeroMonsters' :: (Ord a, Num c) => (a, a) -> (c, Map a Natural) -> (c, Map a Natural)
+killNonZeroMonsters' (monsterHp, monsterSword) (numKilled, currentSwords) =
+  maybe
+    (numKilled, currentSwords)
+    ( (,)
+        (numKilled + 1)
+        . (($) <$> removeSword <*> (flip addSword currentSwords . max monsterSword))
+        . fst
+    )
+    (Map.lookupGE monsterHp currentSwords)
+
+killNonZeroMonsters :: (Ord a, Num c) => Map a Natural -> [(a, a)] -> (c, Map a Natural)
+killNonZeroMonsters swords = foldr killNonZeroMonsters' (0, swords)
+
+killMonsters :: (Ord a, Num a, Num c) => Map a Natural -> [(a, a)] -> c
+killMonsters swords monsters = numKilled + zeroMonstersKilled
+  where
+    (nonZeroMonsters, monstersZeroSword) = partition ((/= 0) . snd) monsters
+    (numKilled, remainingSwords) = killNonZeroMonsters swords $ sortOn Down nonZeroMonsters
+    zeroMonstersKilled = killMonstersZeroSword remainingSwords $ fst <$> monstersZeroSword
 
 main :: IO ()
 main = do
   t <- readLn
   replicateM_ t $ do
     _ <- getLine
-    as <- sort . map read . words <$> getLine
-    bs <- map read . words <$> getLine
-    cs <- (map read . words <$> getLine) :: IO [Int]
-    print $ killMonsters (maximum as) (sortOn fst (zip bs cs))
+    as <- (fst . fromJust . C.readInt <$>) . C.words <$> C.getLine
+    bs <- (fst . fromJust . C.readInt <$>) . C.words <$> C.getLine
+    cs <- (fst . fromJust . C.readInt <$>) . C.words <$> C.getLine
+    print
+      . killMonsters (Map.fromListWith (+) [(a, 1) | a <- as])
+      $ zip bs cs
