e4c7ad73770b08525bf7ebffc8d2a8917d87e13d
Kawin Nikomborirak <knikomborirak@icloud.com>
2025-12-17 21:01:47 -0800

    refactor: rename and update logic functions for clarity

--- src/contest2173/a.hs
+++ src/contest2173/a.hs
@@ -1,17 +1,19 @@
 import Control.Monad (replicateM_)
 
-logic' :: (Num k, Eq k, Num n) => k -> (k, n) -> Bool -> (k, n)
-logic' k (_, y) True = (k, y)
-logic' _ (0, y) False = (0, y + 1)
-logic' _ (x, y) False = (x - 1, y)
+incrementMaxSleep :: (Num k, Eq k, Num n) => k -> (k, n) -> Bool -> (k, n)
+incrementMaxSleep k (_, y) True = (k, y)
+incrementMaxSleep _ (0, y) False = (0, y + 1)
+incrementMaxSleep _ (x, y) False = (x - 1, y)
 
-logic :: (Foldable f, Num k, Eq k, Num n) => k -> f Bool -> n
-logic k = snd . foldl (logic' k) (0, 0)
+maxSleeps :: (Foldable f, Num k, Eq k, Num n) => k -> f Bool -> n
+maxSleeps = (snd .) . flip foldl (0, 0) . incrementMaxSleep
 
 main :: IO ()
-main = do
-  t <- readLn
-  replicateM_ t $ do
-    _ : k : _ <- map read . words <$> getLine
-    s <- map (== '1') <$> getLine
-    print $ logic k s
+main =
+  readLn
+    >>= flip
+      replicateM_
+      ( getLine
+          >>= (<$> getLine) . (. fmap (== '1')) . maxSleeps . (!! 1) . fmap read . words
+          >>= print
+      )
