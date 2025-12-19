9eb7b32c28799b3ee33c9a4bd6d901308e365bf0
Kawin Nikomborirak <knikomborirak@icloud.com>
2025-12-18 21:47:44 -0800

    refactor: simplify input handling and improve accumFindMaxScore logic

--- src/contest2173/b.hs
+++ src/contest2173/b.hs
@@ -1,31 +1,20 @@
 import Control.Monad (replicateM_)
-import qualified Data.ByteString.Char8 as C
-import Data.Maybe (fromJust)
 
 accumFindMaxScore :: (Num n, Ord n) => (n, n) -> (n, n) -> (n, n)
-accumFindMaxScore (a, b) (c, d) = (min (minAB - c) (d - maxAB), max (maxAB - c) (d - minAB))
-  where
-    maxAB = max a b
-    minAB = min a b
+accumFindMaxScore (a, b) (c, d) = (min (a - c) (d - b), max (b - c) (d - a))
 
 findMaxScore :: (Foldable f, Num n, Ord n) => f (n, n) -> n
 findMaxScore = snd . foldl accumFindMaxScore (0, 0)
 
 main :: IO ()
 main =
-  C.getLine
+  readLn
     >>= flip
       replicateM_
-      ( C.getLine
-          *> ( (. fmap (fst . fromJust . C.readInt) . C.words)
-                 . zip
-                 . fmap (fst . fromJust . C.readInt)
-                 . C.words
-                 <$> C.getLine
-                 <*> C.getLine
-                 >>= print . findMaxScore
+      ( getLine
+          *> ( (. fmap read . words) . zip . fmap read . words
+                 <$> getLine
+                 <*> getLine
              )
+          >>= print . findMaxScore
       )
-      . fst
-      . fromJust
-      . C.readInt
